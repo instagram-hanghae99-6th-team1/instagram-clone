dc7022c055de03d179b0c9757e99d40aed4af10f
hyeri-hi5 <merryk555@gmail.com>
2022-04-15 17:00:44 +0900

    초기 프로젝트 세팅

---
+++ README.md
@@ -0,0 +1,55 @@
+# :hearts: Instagram clone
+
+---
+
+프로젝트 설명!
+
+## :sunglasses: Team
+
+인스타그램 클론
+
+---
+
+### Frontend(React)
+
+- 김혜리
+- 박민우
+
+### Backend(Spring)
+
+- ***
+
+### 개발기간
+
+---
+
+2022년 04월 15일 ~2022년 04월 21일
+
+## :earth_asia: Website
+
+---
+
+### :tv: Youtube
+
+## :balloon: Notion Link
+
+---
+
+링크
+
+## :speech_balloon: \***\*Project Collaboration Process\*\***
+
+---
+
+- \***\*Front-end 기술 스택 및 개발 환경\*\***
+
+- \***\*Back-end 기술 스택 및 개발 환경\*\***
+
+---
+
+- **API 설계**
+  | 데이터 테이블 | 기능 | METHOD | URL | request | response | 비고 |
+
+- **Digram**
+- **Troubleshooting**
+- **개인 회고**
---
+++ src/shared/apis.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const api = axios.create({
+  //   baseURL: "변경필요",
+  headers: {
+    "content-type": "application/json;charset=UTF-8",
+    accept: "application/json,",
+  },
+});
+
+api.interceptors.request.use(function (config) {
+  const accessToken = localStorage.Authorization;
+  config.headers.common["Authorization"] = `${accessToken}`;
+  return config;
+});
+
+export const apis = {
+  // post
+  // add: (contents) => api.post("/api/articles", contents),
+  // edit: (id, contents) => api.put(`api/articles/${id}`, contents),
+  delete: (Id) => api.delete(`/api/posts/${Id}`),
+  getDetail: (Id) => api.get(`/api/details/${Id}`),
+
+  // comment
+  addComment: (post_id, NewComment) =>
+    api.post("/api/comment", { postId: post_id, comment: NewComment }),
+  getComment: (post_id) => api.get(`/api/comments/${post_id}`),
+  delComment: (commentId) => api.delete(`/api/comment/${commentId}`),
+  editComment: (commentId, comment) =>
+    api.put(`/api/comment/${commentId}`, { comment }),
+
+  // user
+  login: (id, pwd) => api.post("/user/login", { nickname: id, password: pwd }),
+  // userinfo: () => api.get(`/userinfo`),
+};
